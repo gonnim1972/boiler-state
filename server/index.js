4e879e4f040077d346bdcdc10d77b7c8060649e6
The Core <randyhong@the-core.kr>
2023-01-17 15:20:09 +0900

    ignore file & folder config

--- server/index.js
+++ server/index.js
@@ -50,6 +50,26 @@ app.post('/api/users/register', (req, res) => {
     });
 });
 
+app.post('/api/users/login', (req, res) => {
+    // 요청된 Email을 Database에서 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, userInfo) => {
+        if (!userInfo) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 Email에 해당하는 User가 없습니다."
+            });
+        }
+    });
+
+    // 요청된 Email이 Database에 있다면 비밀번호가 맞는지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+
+    });
+
+    // 비밀번호까지 맞다면 Token을 생성하기.
+
+});
+
 app.listen(port, () => {
     console.log(`boiler-plate app listening on port ${port}!`);
 });
